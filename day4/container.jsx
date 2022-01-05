90b394b60a2349325994060cee7cf373b2dd0043
NGUYEN MINH NHAT DUONG <duongcoilc2004@gmail.com>
2022-01-06 00:17:33 +0700

    todo-app

--- day4/container.jsx
+++ day4/container.jsx
@@ -1,1 +1,10 @@
-ReactDOM.render(<Counter />, document.getElementById("root"));
+const root = document.getElementById("root");
+
+class Container extends React.Component {
+  render() {
+    return <div className="container">
+        <Counter />
+    </div>;
+  }
+}
+ReactDOM.render(<Container />, root);
