9f69ea8c63461d0cf52924400423612055a2a345
NGUYEN MINH NHAT DUONG <duongcoilc2004@gmail.com>
2022-01-06 17:31:34 +0700

    Caculator

--- XO-app/index.jsx
+++ XO-app/index.jsx
@@ -1,10 +1,4 @@
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+
 
 function calculateWinner(squares) {
   const lines = [
