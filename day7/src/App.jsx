cd31514ebec4dde6d170cab57400dfd07998d99d
NGUYEN MINH NHAT DUONG <duongcoilc2004@gmail.com>
2022-01-11 20:35:58 +0700

    git

---
+++ day7/src/App.jsx
@@ -0,0 +1,13 @@
+import { render } from "react-dom";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <i className="bi bi-123"></i>
+      <img style={{width:300}} src="./images/concho.jpgt" />
+    </div>
+  );
+};
+export default App;
